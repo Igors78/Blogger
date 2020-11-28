7d119ff3f9cbad294a9b686b9053c82962074e92
Igors78 <oleinikovsigors@gmail.com>
2020-11-28 15:26:22 +0200

    Correct style with rubocop

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new; end
 
   def show
     @article = Article.find(params[:id])
