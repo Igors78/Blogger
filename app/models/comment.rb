a9e23456615c93a786a268fa69a07f36003b5aca
Igors78 <oleinikovsigors@gmail.com>
2020-11-29 20:25:24 +0200

    Add blank fields validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@
 
 class Comment < ApplicationRecord
   belongs_to :article
+  validates :author, presence: true
+  validates :body, presence: true
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,6 +3,7 @@
 class Tag < ApplicationRecord
   has_many :taggings
   has_many :articles, through: :taggings, dependent: :destroy
+  validates :name, presence: true
   def to_s
     name
   end
