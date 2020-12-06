32bd4c0ab89597b4dafff6028e1f51dbbf23c9ce
Igors78 <oleinikovsigors@gmail.com>
2020-12-06 08:09:12 +0200

    Add custom error messages

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,5 +2,7 @@
 
 class Author < ApplicationRecord
   authenticates_with_sorcery!
-  validates_confirmation_of :password, message: 'Should match confirmation', if: :password
+  validates_confirmation_of :password, message: 'Should match password', if: :password
+  validates :username, :email, :password, presence: true
+  validates :username, :email, uniqueness: true
 end
