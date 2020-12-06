742b0fb9992e823f67a4fd9197fbb169de27d8d8
Igors78 <oleinikovsigors@gmail.com>
2020-12-06 09:00:26 +0200

    Add comment error handling

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :article
-  validates :author_name, presence: true
-  validates :body, presence: true
+  validates :author_name, :body, presence: true
 end
